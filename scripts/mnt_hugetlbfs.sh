6a275e9a31581ef4b10fb1b62cba26353ac6e1fd
Mingyu Gao <gaomy@tsinghua.edu.cn>
2019-10-30 15:13:04 +0800

    [scripts/mnt_hugetlbfs] Add mount huge pages.

---
+++ scripts/mnt_hugetlbfs.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+# Mount/unmount huge pages of memory.
+# Required package: libhugetlbfs-dev
+
+mntpnt=/tmp/mnt-hugetlbfs
+
+mnt() {
+    mkdir -p ${mntpnt}
+    sudo mount -t hugetlbfs none ${mntpnt} -o uid=$(id -u),gid=$(id -g)
+    echo "Mount ${mntpnt} as type hugetlbfs"
+}
+
+umt() {
+    if $( mountpoint -q ${mntpnt} ); then
+        sudo umount ${mntpnt}
+        sleep 1   # wait for umount to finish, avoid device busy error
+        rm -rf ${mntpnt}
+        echo "Unmount ${mntpnt}"
+    fi
+}
+
+if [ "$#" -eq 1 ] && ( [ "$1" == "-m" ] || [ "$1" == "-mount" ] ); then
+    mnt
+elif [ "$#" -eq 1 ] && ( [ "$1" == "-u" ] || [ "$1" == "-umount" ] ); then
+    umt
+else
+    echo "Usage: $0 [-m(ount)] [-u(mount)]" >&2
+    exit 1
+fi
+
